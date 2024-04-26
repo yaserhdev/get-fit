1a521d33b960bfac0845e650424d431b1d9a8be1
Raja Yaser Hussain <yaser@Yasers-MBP.fios-router.home>
2024-04-25 21:22:26 -0400

    Updated User model and created Workout and Category models; updated categorySeeds and seed.js files; updated index.js file in models

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,13 @@
 // index.js (models folder)
 
 const User = require('./User');
+const Workout = require('./Workout');
 const Exercise = require('./Exercise');
+const Category = require('./Category');
 
 module.exports = {
   User,
+  Workout,
   Exercise,
+  Category,
 };
