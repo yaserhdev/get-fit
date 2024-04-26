9f57a3e72e1af24301d096c8df7482aa174b674e
Tyler Clark <tylerc111@gmail.com>
2024-04-25 20:49:20 -0400

    deleted typeDefs from mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,49 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
-    exercises: [Exercise]!
-  }
-
-  type Exercise {
-    _id: ID
-    name: String
-    reps: Int
-    weight: Float
-    sets: Int
-  }
-
-  type AuthPayload {
-    token: String
-    user: User
-  }
-
-  input LoginInput {
-    email: String!
-    password: String!
-  }
-
-  input SignupInput {
-    username: String!
-    email: String!
-    password: String!
-  }
-
-  type Query {
-    # Define any queries here
-  }
-
-  type Mutation {
-    login(input: LoginInput!): AuthPayload
-    signup(input: SignupInput!): AuthPayload
-    # Define other mutations here
-  }
-`;
-
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
