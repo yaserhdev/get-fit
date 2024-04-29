dfc371bc717ce1c7b50f46cbcac3c1d2cbd249c7
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-04-29 18:43:44 -0400

    Changed exercise in user model to workout

--- server/models/User.js
+++ server/models/User.js
@@ -20,10 +20,10 @@ const userSchema = new Schema({
     required: true,
     minLength: 8,
   },
-  exercises: [
+  workouts: [
     {
       type: Schema.Types.ObjectId,
-      ref: 'Exercise',
+      ref: 'Workout',
     },
   ],
 });
