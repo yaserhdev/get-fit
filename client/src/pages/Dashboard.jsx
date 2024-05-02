11781b5c657fc47cb2232349204f2933fda5de99
Austin Luper <austin.luper@gmail.com>
2024-05-02 19:49:05 -0400

    Added Navbar to Dashboard.jsx page, and some other placeholder stuff for the time being

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,27 @@
-// import { useState, useEffect } from 'React';
-// import { useMutation, useQuery } from 'React';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components'; // Import styled-components library
+
+// Import Navbar component
+import Navbar from '../components/Navbar'; 
+
+// Uncommented imports
+// import { useState, useEffect } from 'react';
+// import { useMutation, useQuery } from 'react';
 // import Auth from '../utils/auth';
 
-const Dashboard = () => {
-return (<div>Dashboard</div>)
-}
+// Styled Navbar component with the same styling as Banner
+const StyledDashboard = styled.div`
+  /* Add any styling specific to the Dashboard page here */
+`;
 
+const Dashboard = () => {
+  return (
+    <StyledDashboard>
+      <Navbar /> {/* Include the Navbar component */}
+      <div>Dashboard</div>
+    </StyledDashboard>
+  );
+}
 
 export default Dashboard;
