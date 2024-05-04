7997a15a445326609f5670c40f7b9ac7eb272797
Austin Luper <austin.luper@gmail.com>
2024-05-03 22:15:38 -0400

    Created LogoutButton.jsx with necessary logic for ending the user session and routing the user back to the Login page. Added LogoutButton to the Navbar. Changed main.jsx routing to accommodate logout functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import LogoutButton from './LogoutButton'; // Import the LogoutButton component
 
 const colors = {
   darkGreen: '#010400',
   offWhite: '#FFFBFC',
 };
+
 const StyledNavbar = styled.nav`
   padding: 20px 0; /* Add padding for spacing */
   background-color: ${colors.darkGreen}; /* Dark green background */
@@ -14,21 +16,28 @@ const StyledNavbar = styled.nav`
   width: 100%; /* Make the navbar span the full width of the page */
   top: 0; /* Position the navbar at the very top of the page */
 `;
+
 const StyledUl = styled.ul`
   list-style-type: none; /* Remove bullet points */
   margin: 0;
   padding: 0;
 `;
+
 const StyledLi = styled.li`
   display: inline; /* Display links horizontally */
   margin-right: 20px; /* Add spacing between links */
 `;
+
 const StyledLink = styled(Link)`
   color: ${colors.offWhite}; /* Off-white text color */
   text-decoration: none; /* Remove underline */
   font-size: 18px; /* Adjust font size */
   font-weight: bold; /* Bold font weight */
   text-transform: uppercase; /* Capitalize all text */
+`;
+
+const LogoutButtonContainer = styled.div`
+  margin-left: auto; /* Align to the right */
 `;
 
 const Navbar = () => {
@@ -42,6 +51,9 @@ const Navbar = () => {
           <StyledLink to="/profile">Profile</StyledLink>
         </StyledLi>
       </StyledUl>
+      <LogoutButtonContainer>
+        <LogoutButton /> {/* Include the LogoutButton component */}
+      </LogoutButtonContainer>
     </StyledNavbar>
   );
 };
