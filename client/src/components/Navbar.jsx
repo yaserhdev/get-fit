d3491119500040ca9c9ace7f064983cc89c8e907
Austin Luper <austin.luper@gmail.com>
2024-05-03 22:51:57 -0400

    Moved the Logout clickable text in the Navbar to the right and it still seems to work

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import LogoutButton from './LogoutButton'; // Import the LogoutButton component
+import AuthService from '../utils/auth'; // Import AuthService for logout functionality
 
 const colors = {
   darkGreen: '#010400',
@@ -15,12 +15,18 @@ const StyledNavbar = styled.nav`
   position: fixed; /* Position the navbar at the top of the viewport */
   width: 100%; /* Make the navbar span the full width of the page */
   top: 0; /* Position the navbar at the very top of the page */
+  display: flex; /* Use flexbox */
+  justify-content: space-between; /* Space elements evenly */
+  align-items: center; /* Align items vertically */
 `;
 
 const StyledUl = styled.ul`
   list-style-type: none; /* Remove bullet points */
   margin: 0;
   padding: 0;
+  flex: 1; /* Grow to take remaining space */
+  display: flex; /* Use flexbox */
+  justify-content: center; /* Center align items horizontally */
 `;
 
 const StyledLi = styled.li`
@@ -36,11 +42,25 @@ const StyledLink = styled(Link)`
   text-transform: uppercase; /* Capitalize all text */
 `;
 
-const LogoutButtonContainer = styled.div`
-  margin-left: auto; /* Align to the right */
+const StyledButton = styled.button`
+  color: ${colors.offWhite}; /* Off-white text color */
+  background-color: transparent; /* Transparent background */
+  border: none; /* Remove border */
+  font-size: 18px; /* Adjust font size */
+  font-weight: bold; /* Bold font weight */
+  text-transform: uppercase; /* Capitalize all text */
+  cursor: pointer; /* Show pointer cursor on hover */
+  margin-right: 25px; /* Add margin to the right */
 `;
 
 const Navbar = () => {
+  const handleLogout = () => {
+    // Call the logout method from AuthService
+    AuthService.logout();
+    // Redirect the user back to the login screen
+    window.location.href = '/login';
+  };
+
   return (
     <StyledNavbar>
       <StyledUl>
@@ -51,9 +71,7 @@ const Navbar = () => {
           <StyledLink to="/profile">Profile</StyledLink>
         </StyledLi>
       </StyledUl>
-      <LogoutButtonContainer>
-        <LogoutButton /> {/* Include the LogoutButton component */}
-      </LogoutButtonContainer>
+      <StyledButton onClick={handleLogout}>Logout</StyledButton> {/* Styled logout button */}
     </StyledNavbar>
   );
 };
