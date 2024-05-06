afc8e4f18564ea76ad376c1a69ef3112b3641c24
Raja Yaser Hussain <yaser@Yasers-MBP.fios-router.home>
2024-05-06 15:36:51 -0400

    Added edit workout button and logic for handling

--- client/src/components/MyWorkouts.jsx
+++ client/src/components/MyWorkouts.jsx
@@ -1,6 +1,6 @@
 import { useMutation, useQuery } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
-import { REMOVE_WORKOUT } from '../utils/mutations';
+import { REMOVE_WORKOUT, UPDATE_WORKOUT } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { Link } from 'react-router-dom';
 
@@ -9,6 +9,7 @@ const MyWorkouts = () => {
   const userData = data?.me;
   console.log(userData);
   const [removeWorkout] = useMutation(REMOVE_WORKOUT);
+  const [updateWorkout] = useMutation(UPDATE_WORKOUT);
 
   const handleDeleteWorkout = async (workoutId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -24,6 +25,20 @@ const MyWorkouts = () => {
       console.error(err);
     }
   };
+
+  const handleUpdateWorkout = async (workoutId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      return false;
+    }
+    try {
+      const { data } = await updateWorkout({
+        variables: { workoutId },
+      })
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   if (loading) {
     return <h2>LOADING...</h2>;
@@ -41,6 +56,9 @@ const MyWorkouts = () => {
             <p>Created By: {workout.workoutAuthor}</p>
             <p>Description: {workout.description}</p>
             <p>Created At: {workout.createdAt}</p>
+            <button className="edit-workout-btn" onClick={() => handleUpdateWorkout(workout._id)}>
+              Edit this workout!
+            </button>
             <button className="delete-workout-btn" onClick={() => handleDeleteWorkout(workout._id)}>
               Delete this workout!
             </button>
